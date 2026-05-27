6eede6eec94623f329659fa323b1de9d46fc07c4
Michael-o22 <Suphalerk.chur@gmail.com>
2026-05-27 18:03:28 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Fill this object with your Firebase web app config.
+// Keep the variable name UAV_FIREBASE_CONFIG unchanged.
+window.UAV_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDNgwRyjhsPPSCyWx7nEXsAep3HfUZr9fE",
+  authDomain: "uavplaner-04.firebaseapp.com",
+  projectId: "uavplaner-04",
+  storageBucket: "uavplaner-04.firebasestorage.app",
+  messagingSenderId: "338437574571",
+  appId: "1:338437574571:web:4f9675459ceef2e5000cee"
+};
